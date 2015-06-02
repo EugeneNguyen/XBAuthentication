f3a0fa15a11d31f07883360e63bbfbb34ed97172
eugenenguyen <xuanbinh91@gmail.com>
2015-06-02 13:54:11 +0700

    Quick fix

--- Example/Pods/Target Support Files/Pods-XBAuthentication/Pods-XBAuthentication-environment.h
+++ Example/Pods/Target Support Files/Pods-XBAuthentication/Pods-XBAuthentication-environment.h
@@ -10,43 +10,49 @@
 #define COCOAPODS_POD_AVAILABLE_AFNetworking
 #define COCOAPODS_VERSION_MAJOR_AFNetworking 2
 #define COCOAPODS_VERSION_MINOR_AFNetworking 5
-#define COCOAPODS_VERSION_PATCH_AFNetworking 3
+#define COCOAPODS_VERSION_PATCH_AFNetworking 4
 
 // AFNetworking/NSURLConnection
 #define COCOAPODS_POD_AVAILABLE_AFNetworking_NSURLConnection
 #define COCOAPODS_VERSION_MAJOR_AFNetworking_NSURLConnection 2
 #define COCOAPODS_VERSION_MINOR_AFNetworking_NSURLConnection 5
-#define COCOAPODS_VERSION_PATCH_AFNetworking_NSURLConnection 3
+#define COCOAPODS_VERSION_PATCH_AFNetworking_NSURLConnection 4
 
 // AFNetworking/NSURLSession
 #define COCOAPODS_POD_AVAILABLE_AFNetworking_NSURLSession
 #define COCOAPODS_VERSION_MAJOR_AFNetworking_NSURLSession 2
 #define COCOAPODS_VERSION_MINOR_AFNetworking_NSURLSession 5
-#define COCOAPODS_VERSION_PATCH_AFNetworking_NSURLSession 3
+#define COCOAPODS_VERSION_PATCH_AFNetworking_NSURLSession 4
 
 // AFNetworking/Reachability
 #define COCOAPODS_POD_AVAILABLE_AFNetworking_Reachability
 #define COCOAPODS_VERSION_MAJOR_AFNetworking_Reachability 2
 #define COCOAPODS_VERSION_MINOR_AFNetworking_Reachability 5
-#define COCOAPODS_VERSION_PATCH_AFNetworking_Reachability 3
+#define COCOAPODS_VERSION_PATCH_AFNetworking_Reachability 4
 
 // AFNetworking/Security
 #define COCOAPODS_POD_AVAILABLE_AFNetworking_Security
 #define COCOAPODS_VERSION_MAJOR_AFNetworking_Security 2
 #define COCOAPODS_VERSION_MINOR_AFNetworking_Security 5
-#define COCOAPODS_VERSION_PATCH_AFNetworking_Security 3
+#define COCOAPODS_VERSION_PATCH_AFNetworking_Security 4
 
 // AFNetworking/Serialization
 #define COCOAPODS_POD_AVAILABLE_AFNetworking_Serialization
 #define COCOAPODS_VERSION_MAJOR_AFNetworking_Serialization 2
 #define COCOAPODS_VERSION_MINOR_AFNetworking_Serialization 5
-#define COCOAPODS_VERSION_PATCH_AFNetworking_Serialization 3
+#define COCOAPODS_VERSION_PATCH_AFNetworking_Serialization 4
 
 // AFNetworking/UIKit
 #define COCOAPODS_POD_AVAILABLE_AFNetworking_UIKit
 #define COCOAPODS_VERSION_MAJOR_AFNetworking_UIKit 2
 #define COCOAPODS_VERSION_MINOR_AFNetworking_UIKit 5
-#define COCOAPODS_VERSION_PATCH_AFNetworking_UIKit 3
+#define COCOAPODS_VERSION_PATCH_AFNetworking_UIKit 4
+
+// AVHexColor
+#define COCOAPODS_POD_AVAILABLE_AVHexColor
+#define COCOAPODS_VERSION_MAJOR_AVHexColor 2
+#define COCOAPODS_VERSION_MINOR_AVHexColor 0
+#define COCOAPODS_VERSION_PATCH_AVHexColor 0
 
 // Bolts
 #define COCOAPODS_POD_AVAILABLE_Bolts
@@ -131,18 +137,25 @@
 // This library does not follow semantic-versioning,
 // so we were not able to define version macros.
 // Please contact the author.
-// Version: 0.3.0.1.
+// Version: 0.3.2.1.
 
 // XBCacheRequest
 #define COCOAPODS_POD_AVAILABLE_XBCacheRequest
-#define COCOAPODS_VERSION_MAJOR_XBCacheRequest 0
-#define COCOAPODS_VERSION_MINOR_XBCacheRequest 5
-#define COCOAPODS_VERSION_PATCH_XBCacheRequest 3
-
-// XBLanguage
-#define COCOAPODS_POD_AVAILABLE_XBLanguage
 // This library does not follow semantic-versioning,
 // so we were not able to define version macros.
 // Please contact the author.
-// Version: 0.5.1.1.
+// Version: 0.5.3.2.
+
+// XBExtension
+#define COCOAPODS_POD_AVAILABLE_XBExtension
+// This library does not follow semantic-versioning,
+// so we were not able to define version macros.
+// Please contact the author.
+// Version: 0.2.2.1.
+
+// XBLanguage
+#define COCOAPODS_POD_AVAILABLE_XBLanguage
+#define COCOAPODS_VERSION_MAJOR_XBLanguage 1
+#define COCOAPODS_VERSION_MINOR_XBLanguage 0
+#define COCOAPODS_VERSION_PATCH_XBLanguage 0
 
